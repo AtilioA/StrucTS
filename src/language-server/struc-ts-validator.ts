c9f39014abfa309d28705a585e589ee38a10cf97
AtilioA <atiliogpg@protonmail.com>
2023-04-12 18:49:53 -0300

    First commit

---
+++ src/language-server/struc-ts-validator.ts
@@ -0,0 +1,31 @@
+import { ValidationAcceptor, ValidationChecks } from 'langium';
+import { StrucTsAstType, Person } from './generated/ast';
+import type { StrucTsServices } from './struc-ts-module';
+
+/**
+ * Register custom validation checks.
+ */
+export function registerValidationChecks(services: StrucTsServices) {
+    const registry = services.validation.ValidationRegistry;
+    const validator = services.validation.StrucTsValidator;
+    const checks: ValidationChecks<StrucTsAstType> = {
+        Person: validator.checkPersonStartsWithCapital
+    };
+    registry.register(checks, validator);
+}
+
+/**
+ * Implementation of custom validations.
+ */
+export class StrucTsValidator {
+
+    checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
+        if (person.name) {
+            const firstChar = person.name.substring(0, 1);
+            if (firstChar.toUpperCase() !== firstChar) {
+                accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
+            }
+        }
+    }
+
+}
