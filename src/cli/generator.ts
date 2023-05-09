a23c144231cdef1ff6a5d6ec3c13032502a0bbce
AtilioA <atiliogpg@protonmail.com>
2023-05-09 19:32:52 -0300

    feat: start generating TypeScript code from AST
    create initial version of generation function generateClass()`, stub for generateProperty();
    create generate action for the cli script

--- src/cli/generator.ts
+++ src/cli/generator.ts
@@ -1,20 +1,57 @@
 import fs from 'fs';
-import { CompositeGeneratorNode, NL, toString } from 'langium';
+import { GeneratorNode, CompositeGeneratorNode, NL, toString } from 'langium';
 import path from 'path';
-import { Model } from '../language-server/generated/ast';
+import { Class, Model, isClass } from '../language-server/generated/ast';
 import { extractDestinationAndName } from './cli-util';
 
-export function generateTypeScript(model: Model, filePath: string, destination: string | undefined): string {
-    const data = extractDestinationAndName(filePath, destination);
-    const generatedFilePath = `${path.join(data.destination, data.name)}.ts`;
+// Stub generateProperty function
+export function generateProperty(property: unknown): GeneratorNode {
+    return new CompositeGeneratorNode();
+}
 
+export function generateClass(cls: Class): CompositeGeneratorNode {
+  const classNode = new CompositeGeneratorNode();
+
+  // Class header
+  classNode.append("export class ", cls.name, " {", NL);
+
+  // Iterate through the properties of the class and generate code for each property
+  for (const property of cls.properties) {
+    // const propertyNode = generateProperty(property);
+    classNode.append(property.name, NL);
+  }
+
+  // Class 'footer'
+  classNode.append("}", NL);
+
+  return classNode;
+}
+
+export function generateTypeScript(model: Model, filePath: string, destination: string | undefined): string {
     const fileNode = new CompositeGeneratorNode();
     fileNode.append('"use strict";', NL, NL);
-    // model.greetings.forEach(greeting => fileNode.append(`console.log('Hello, ${greeting.person.ref?.name}!');`, NL));
+
+    // Iterate through the top-level elements in the Model (AST)
+    for (const element of model.elements) {
+        if (isClass(element)) {
+            const classNode = generateClass(element);
+            fileNode.append(classNode, NL);
+        }
+    }
+
+    return toString(fileNode);
+}
+
+export function generateCommands(model: Model, filePath: string, destination: string | undefined): string {
+    const data = extractDestinationAndName(filePath, destination);
+    const generatedFilePath = `${path.join(data.destination, data.name)}.ts`;
 
     if (!fs.existsSync(data.destination)) {
         fs.mkdirSync(data.destination, { recursive: true });
     }
-    fs.writeFileSync(generatedFilePath, toString(fileNode));
+
+    const result = generateTypeScript(model, filePath, destination);
+
+    fs.writeFileSync(generatedFilePath, result);
     return generatedFilePath;
 }
--- src/cli/index.ts
+++ src/cli/index.ts
@@ -4,14 +4,14 @@ import { Model } from '../language-server/generated/ast';
 import { StrucTsLanguageMetaData } from '../language-server/generated/module';
 import { createStrucTsServices } from '../language-server/struc-ts-module';
 import { extractAstNode } from './cli-util';
-import { generateJavaScript } from './generator';
+import { generateCommands } from './generator';
 import { NodeFileSystem } from 'langium/node';
 
 export const generateAction = async (fileName: string, opts: GenerateOptions): Promise<void> => {
     const services = createStrucTsServices(NodeFileSystem).StrucTs;
     const model = await extractAstNode<Model>(fileName, services);
-    const generatedFilePath = generateJavaScript(model, fileName, opts.destination);
-    console.log(chalk.green(`JavaScript code generated successfully: ${generatedFilePath}`));
+    const generatedFilePath = generateCommands(model, fileName, opts.destination);
+    console.log(chalk.green(`TypeScript code generated successfully: ${generatedFilePath}`));
 };
 
 export type GenerateOptions = {
@@ -30,7 +30,7 @@ export default function(): void {
         .command('generate')
         .argument('<file>', `source file (possible file extensions: ${fileExtensions})`)
         .option('-d, --destination <dir>', 'destination directory of generating')
-        .description('generates JavaScript code that prints "Hello, {name}!" for each greeting in a source file')
+        .description('generates TypeScript code from a StrucTS input file')
         .action(generateAction);
 
     program.parse(process.argv);
