c80d5e875cdb08c23a8d31b3659c8175f0725067
AtilioA <atiliogpg@protonmail.com>
2023-05-26 16:21:36 -0300

    refactor(back-end): generate `constructor`, fix parameters, modularize generators

---
+++ src/cli/utils/strings.ts
@@ -0,0 +1,22 @@
+import { type CARDINALITY_LIMIT, type Property, isAttributeProperty } from '../../language-server/generated/ast';
+
+// eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
+export function convertCardinalityLimitToValue(limit: CARDINALITY_LIMIT | undefined): string | CARDINALITY_LIMIT | undefined {
+	if (limit === '*') {
+		return 'null';
+	}
+
+	return limit;
+}
+
+export function createCollectionString(property: Property) {
+	const lowerLimit = convertCardinalityLimitToValue(property?.cardinality?.lower);
+	const upperLimit = convertCardinalityLimitToValue(property?.cardinality?.upper);
+
+	if (isAttributeProperty(property)) {
+		return `new CustomCollection<${property.type}>(${lowerLimit}, ${upperLimit});`;
+	} else {
+		// Composition or reference
+		return `new CustomCollection<${property.type.class.ref?.name}>(${lowerLimit}, ${upperLimit});`;
+	}
+}
