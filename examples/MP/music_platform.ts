e3a7a884048c3a654517cebfb4dc7e3392fb315c
AtilioA <atiliogpg@protonmail.com>
2023-06-16 18:33:56 -0300

    fix(docs): update and simplify MP example

--- examples/MP/music_platform.ts
+++ examples/MP/music_platform.ts
@@ -2,80 +2,7 @@
 // We will use this implementation to create a collection of songs for an album
 // A collection can have a minimum number of items and a maximum number of items.
 
-// The generic type (T) will be used to specify the type of item that can be added to the collection. Must not be exported.
-class CustomCollection<T> {
-	// The items in the collection are stored in an array.
-	private readonly items: T[];
-
-	// The constructor takes the minimum number of items and the maximum number of items as parameters.
-	// The maxItems parameter is optional and can be null, meaning that there is no maximum number of items.
-	constructor(private readonly minItems: number, private readonly maxItems: number | null) {
-		// The items array is initialized to an empty array.
-		this.items = [];
-	}
-
-	/**
-   * Add an item to the collection.
-   *
-   * @param item The item to add
-   * @return true if the item was added; false otherwise
-   * @throws An error if the cardinality constraint is violated (i.e. the collection would have more than the maximum number of items)
-   */
-	add(item: T): boolean {
-		// If the collection has no maximum number of items, or the number of items in the collection is less than the maxItems ('not full'),
-		if (this.maxItems === null || this.items.length < this.maxItems) {
-			// The item is added to the items array.
-			this.items.push(item);
-			// The method returns true to indicate that the item was added to the collection.
-			return true;
-			// If the maxItems is not null and the number of items in the collection is greater than or equal to the maxItems, the item cannot be added to the collection.
-		}
-
-		// An error is thrown to indicate that the item could not be added to the collection due to the cardinality constraint.
-		throw new Error(
-			`Adding this item would violate the cardinality constraint: Collection cannot have more than ${this.maxItems} items.`,
-		);
-	}
-
-	/**
-   * Remove an item from the collection.
-   *
-   * @param item The item to remove
-   * @returns true if the item was removed; false otherwise
-   * @throws An error if the cardinality constraint is violated (i.e. the collection would have less than the minimum number of items)
-   */
-	remove(item: T): boolean {
-		// Search for the item in the items array.
-		const index = this.items.indexOf(item);
-		// If the index is not -1, the item was found in the collection.
-		if (index === -1) {
-			// An error is thrown to indicate that the item could not be removed from the collection.
-			throw new Error(
-				`The ${typeof item} to be removed does not exist in the collection, so it cannot be removed.`,
-			);
-		} else {
-			// The item is removed from this collection's items array.
-			this.items.splice(index, 1);
-			// If the number of items in the collection is less than the minItems, the item cannot be removed from the collection.
-			if (this.items.length < this.minItems) {
-				// An error is thrown to indicate that the item could not be removed from the collection, due to the cardinality constraint.
-				throw new Error(
-					`Removing this ${typeof item} would violate the cardinality constraint: ${this.constructor.name} cannot have less than ${this.minItems} items.`,
-				);
-				// If the number of items in the collection is greater than or equal to the minItems, the item can be removed from the collection.
-			} else {
-				// The method returns true to indicate that the item was removed from the collection.
-				return true;
-			}
-			// If the index is -1, the item was not even found in the collection.
-		}
-	}
-
-	// This method returns the items in the collection.
-	getItems(): T[] {
-		return this.items;
-	}
-}
+import { CustomCollection } from '../../generated/strucTS_core/custom_collection';
 
 // Declare the Song class
 export class Song {
@@ -187,5 +114,73 @@ export class Album {
 	getSongs(): Song[] {
 		// Use the CustomCollection.getItems() method to polymorphically get the items in the collection
 		return this._songs.getItems();
+	}
+}
+
+export class Playlist {
+	name: string;
+	songs: CustomCollection<Song>;
+
+	constructor() {
+		this.name = '';
+		this.songs = new CustomCollection<Song>(0, null);
+	}
+
+	/**
+	 * Add a song to the playlist.
+	 * @param song The song to add
+	 * @returns true if the song was added; false otherwise;
+	 */
+	addSong(song: Song): boolean {
+		return this.songs.add(song);
+	}
+
+	/**
+	 * Remove a song from the playlist.
+	 * @param song The song to remove
+	 * @returns true if the song was removed; false otherwise; throws an error if cardinality constraint is violated)
+	 */
+	removeSong(song: Song): boolean {
+		return this.songs.remove(song);
+	}
+
+	/**
+	 * Get the songs in this playlist.
+	 * @returns The CustomCollection for songs in this playlist
+	 */
+	getSongs(): CustomCollection<Song> {
+		return this.songs;
+	}
+}
+
+/**
+ * Builder class for Playlist objects.
+ * This class is able to build objects of type Playlist by setting its properties one by one.
+ */
+export class PlaylistBuilder {
+	private playlist: Playlist;
+
+	constructor() {
+		this.reset();
+	}
+
+	public reset(): void {
+		this.playlist = new Playlist();
+	}
+
+	public setName(name: string): this {
+		this.playlist.name = name;
+		return this;
+	}
+
+	public addSong(song: Song): this {
+		this.playlist.addSong(song);
+		return this;
+	}
+
+	public build(): Playlist {
+		const result = this.playlist;
+		this.reset();
+		return result;
 	}
 }
